f80925855c7b1dfb0d4436f1d190452246a919e4
Fred <frederic.Flaceliere@gmail.com>
2023-01-20 16:31:51 +0100

    read/write and parser file added v2

---
+++ src/main/scala/parser/inputParser.scala
@@ -0,0 +1,9 @@
+package fr.esgi.project
+package parser
+
+import error.BadDataException
+import Structure.{Orientation, Instruction, FileType}
+
+object InputParser {
+
+}
---
+++ src/main/scala/parser/outputParser.scala
@@ -0,0 +1,10 @@
+package fr.esgi.project
+package parser
+
+import error.BadDataException
+import Structure.{Orientation, Instruction, FileType}
+
+object OutputParser {
+
+}
+
