4f2143ab48e13dd2cd5c53e61f43722551170bbc
Fred <frederic.Flaceliere@gmail.com>
2023-01-20 16:31:27 +0100

    read/write and parser file added

--- src/main/scala/error/error.scala
+++ src/main/scala/error/error.scala
@@ -1,7 +1,5 @@
 package fr.esgi.project
 package error
 
-package kernel.error
-
 final case class BadDataException(message: String = "", private val cause: Throwable = None.orNull)
   extends Exception(message, cause)
